e7e8b890c8791f7bff6d7cfb5f7d074ee75fdb2f
chaudhuree <67362262+chaudhuree@users.noreply.github.com>
2023-07-21 16:58:21 +0600

    Initial commit

---
+++ src/demo.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import './index.css';
+import { Button, Modal } from 'antd';
+const App = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    // Save the current URL to revert back to it later
+    window.history.pushState(null, null, window.location.pathname);
+    // Update the URL to the new one for the modal
+    window.history.pushState(null, null, '/modal');
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    // revert back to the previous url
+    window.history.back();
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    // revert back to the previous url
+    window.history.back();
+    setIsModalOpen(false);
+  };
+  return (
+    <>
+      <Button type="primary" onClick={showModal}>
+        Open Modal
+      </Button>
+      <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+        text ever since the 1500s,</p>
+      </Modal>
+    </>
+  );
+};
+export default App;
